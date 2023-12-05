717f7e7c1bfb6c81e73373912941298ca9983167
Saswata Paul <saspaul2014@gmail.com>
2023-12-05 20:24:13 +0530

    now new divs are created by automatically when the form is submitted

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 document.getElementById('submitButton').addEventListener('click', function(event){
     event.preventDefault();
     handleFormData();
-    hideForm();
 } );
 
 function showForm()
@@ -25,8 +24,55 @@ function handleFormData()
     var pageNumber = document.getElementById("numberOfPages").value;
     var radioChecked = document.querySelector('input[name="yes_no"]:checked');
     var readTheBookOrNot = radioChecked ? radioChecked.value : "Not Specified";
-    books.push(new Book (bookName,authorName,pageNumber,readTheBookOrNot));
-    console.log(books[books.length - 1]);
+    
+    var newBook = new Book(bookName, authorName, pageNumber, readTheBookOrNot);
+    books.push(newBook);
+    createBookElement(newBook);
+    hideForm();
+}
+
+function createBookElement(book)
+{
+    
+    var bookContainer = document.createElement("div");
+    bookContainer.classList.add("book");
+
+    var detailsContainer = document.createElement("div");
+    detailsContainer.classList.add("book-details");
+
+    var titleParagraph = document.createElement("p");
+    titleParagraph.classList.add("title");
+    titleParagraph.textContent = book.name;
+
+    var authorParagraph = document.createElement("p");
+    authorParagraph.classList.add("author");
+    authorParagraph.textContent = book.author;
+
+    var pageParagraph = document.createElement("p");
+    pageParagraph.classList.add("pages");
+    pageParagraph.textContent = book.page;
+
+    detailsContainer.appendChild(titleParagraph);
+    detailsContainer.appendChild(authorParagraph);
+    detailsContainer.appendChild(pageParagraph);
+
+    var btnContainer = document.createElement("div");
+    btnContainer.classList.add("book-btn");
+
+    var readButton = document.createElement("button");
+    readButton.textContent = "Read";
+
+    var removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+
+    btnContainer.appendChild(readButton);
+    btnContainer.appendChild(removeButton);
+
+    bookContainer.appendChild(detailsContainer);
+    bookContainer.appendChild(btnContainer);
+
+    document.querySelector('.book-template-grid').appendChild(bookContainer);
+
 }
 
 var books = [];
